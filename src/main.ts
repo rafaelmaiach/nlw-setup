dcd078e4313aab647af72b389e87bf0e1651f564
rafaelmaiach <rafael.chieregatto@hotmail.com>
2023-01-16 20:49:26 -0300

    chore: initial setup

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './styles/tailwind.css'
+import './styles/global.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), viteCompression({
+    verbose: true,
+    algorithm: 'brotliCompress',
+  })],
+});
