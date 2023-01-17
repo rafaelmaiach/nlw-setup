5d9a6f49a230d561d4b2cb512e8842995d7ea3b9
rafaelmaiach <rafael.chieregatto@hotmail.com>
2023-01-17 20:34:29 -0300

    style: create classes for habit colors

--- src/components/TrackerHeader/TrackerHeaderLogo.vue
+++ src/components/TrackerHeader/TrackerHeaderLogo.vue
@@ -1,19 +1,13 @@
 <template>
 	<div class="flex flex-col">
 		<div class="flex gap-1 mb-4">
-			<span class="color border-zinc-800 bg-zinc-900" />
-			<span class="color border-violet-700 bg-violet-900" />
-			<span class="color border-violet-600 bg-violet-800" />
-			<span class="color border-violet-500 bg-violet-700" />
-			<span class="color border-violet-400 bg-violet-600" />
-			<span class="color border-violet-300 bg-violet-500" />
+			<span class="square zinc-800" />
+			<span class="square violet-700" />
+			<span class="square violet-600" />
+			<span class="square violet-500" />
+			<span class="square violet-400" />
+			<span class="square violet-300" />
 		</div>
 		<span class="text-5xl font-extrabold">habits</span>
 	</div>
 </template>
-
-<style scoped>
-	.color {
-		@apply h-4 w-4 rounded border;
-	}
-</style>
